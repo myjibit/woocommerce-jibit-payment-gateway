4f5c4bd42495b01cb9ecfef76d08d1fe77fa27fd
Vahidd <vahidd.com@gmail.com>
2018-09-30 13:40:07 +0330

    Filter and validate phone number on checkout page

--- filters.php
+++ filters.php
@@ -6,3 +6,35 @@ function wjpgAddGateway( $methods ) {
 
 	return $methods;
 }
+
+add_action( 'woocommerce_after_checkout_validation', 'wjpgValidatePhoneNumberOnCheckoutPage', 10, 2 );
+
+/**
+ * Validates that the checkout form contains valid phone number
+ *
+ * @param  array    $data   An array of posted data.
+ * @param  WP_Error $errors Validation errors.
+ */
+function wjpgValidatePhoneNumberOnCheckoutPage( $data, $errors ) {
+	$phoneNumber = trim( $data[ 'billing_phone' ] );
+
+	if ( empty( $phoneNumber ) && ! in_array( 'required-field', $errors->get_error_codes() ) ) {
+		$errors->add( 'required-field', __( "Phone number is a required field.", 'wjpg' ) );
+	} elseif ( ! empty( $phoneNumber ) ) {
+		if ( ! preg_match( '/^09\d{9}$/', $phoneNumber ) ) {
+			$errors->add( 'invalid_phone_number', __( "Phone number is invalid.", 'wjpg' ) );
+		}
+	}
+}
+
+add_filter( 'woocommerce_checkout_posted_data', 'wjpgFilterCheckoutPostData' );
+
+function wjpgFilterCheckoutPostData( $data ) {
+	$phone = $data[ 'billing_phone' ];
+
+	if ( preg_match( '/^((00|\+)?(98)|0)?(9\d{9})$/', $phone, $matches ) ) {
+		$data[ 'billing_phone' ] = "0" . $matches[ 4 ];
+	}
+
+	return $data;
+}
